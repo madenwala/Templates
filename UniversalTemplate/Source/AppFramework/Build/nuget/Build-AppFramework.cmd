97a1e5b6e0ee6de07f11e7056a1b3656a03351c2
Mohammed Adenwala <madenwal@microsoft.com>
2017-03-17 23:59:54 -0500

    Enabled XML documentation to be built on release

---
+++ UniversalTemplate/Source/AppFramework/Build/nuget/Build-AppFramework.cmd
@@ -0,0 +1,47 @@
+@ECHO OFF
+REM
+REM Version is read from the VERSION file.
+REM
+REM Say VERSION contains "0.0.3" then:
+REM
+REM build-nupkg                     <-- generates package with version 0.0.3
+REM
+
+SETLOCAL
+
+PUSHD "%~dp0"
+
+WHERE /Q nuget >NUL
+IF %ERRORLEVEL% NEQ 0 (     
+    ECHO ERROR: nuget not found.
+    ECHO.
+    ECHO Run "%~pd0download-nuget.cmd" to download the latest version, or update PATH as appropriate. 
+    GOTO END
+)
+
+
+SET PACKAGENAME=AppFramework
+SET /p VERSION=<VERSION
+SET BIN=bin
+SET OUTDIR=..\..\Output
+ 
+
+IF NOT "%1" == "" (
+    SET VERSION=%VERSION%-%1
+)
+
+SET NUGET_ARGS=^
+    -nopackageanalysis ^
+    -basepath ..\..\ ^
+    -outputdirectory %OUTDIR% ^
+    -version %VERSION% 
+     
+
+nuget pack %PACKAGENAME%.nuspec %NUGET_ARGS%
+    IF %ERRORLEVEL% NEQ 0 GOTO END
+
+ 
+:END
+
+POPD
+EXIT /B %ERRORLEVEL%
