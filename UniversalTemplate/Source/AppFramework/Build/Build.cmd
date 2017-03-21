8da79d4ed9dc1a8e03c757b65aa9e771a9907f28
Mohammed Adenwala <adenwala@outlook.com>
2017-03-21 09:18:07 -0500

    NuGet build updates

--- UniversalTemplate/Source/AppFramework/Build/Build.cmd
+++ UniversalTemplate/Source/AppFramework/Build/Build.cmd
@@ -9,4 +9,10 @@ msbuild ..\AppFramework.UI.Uwp\AppFramework.UI.Uwp.csproj /p:Configuration="Rele
 msbuild ..\AppFramework.Core.Uwp\AppFramework.Core.Uwp.csproj /p:Configuration="Release" /p:Platform="arm"
 msbuild ..\AppFramework.UI.Uwp\AppFramework.UI.Uwp.csproj /p:Configuration="Release" /p:Platform="arm"
 
+xcopy ..\AppFramework.Core.Uwp\bin\x86\release\AppFramework.Core.Uwp.dll Temp\AppFramework.Core.Uwp.dll /Y
+corflags /32BITREQ- Temp\AppFramework.Core.Uwp.dll
+
+xcopy ..\AppFramework.UI.Uwp\bin\x86\release\AppFramework.UI.Uwp.dll Temp\AppFramework.UI.Uwp.dll /Y
+corflags /32BITREQ- Temp\AppFramework.UI.Uwp.dll
+
 Build-NuGet-Package.cmd
