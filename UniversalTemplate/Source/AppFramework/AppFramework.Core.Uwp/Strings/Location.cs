22024d28b076ad73850ba4b1816c5cde2407d290
Mohammed Adenwala <madenwal@microsoft.com>
2017-03-23 11:17:23 -0500

    Removed RESW files

---
+++ UniversalTemplate/Source/AppFramework/AppFramework.Core.Uwp/Strings/Location.cs
@@ -0,0 +1,12 @@
+﻿namespace AppFramework.Core.Strings
+{
+    public static class Location
+    {
+        public const string TextDeterminingLocation = "Finding your location...";
+        public const string TextDistanceAwayDisplay = "{0} {1} away";
+        public const string TextKilomentersLong = "Kilometers";
+        public const string TextKilomentersShort = "km";
+        public const string TextMilesLong = "Miles";
+        public const string TextMilesShort = "mi";
+    }
+}
---
+++ UniversalTemplate/Source/AppFramework/AppFramework.Core.Uwp/Strings/Resources.cs
@@ -0,0 +1,30 @@
+﻿namespace AppFramework.Core.Strings
+{
+    public static class Resources
+    {
+        public const string ApplicationProblemEmailBodyTemplate = "Please describe what you were doing when the problem occurred:\r\n--------------------\r\n\r\n\r\n--------------------";
+        public const string ApplicationProblemEmailSubjectTemplate = "{0} {1} auto-generated problem report";
+        public const string ApplicationProblemPromptMessage = "A problem occurred the last time you ran this application.Would you like to send us an email to report it?";
+        public const string ApplicationProblemPromptTitle = "Problem Report";
+        public const string ApplicationSharingBodyText = "Check out {0}from the Windows Store!\r\n\r\n{1}";
+        public const string ApplicationSupportEmailBodyTemplate = "Please provide and feedback you'd like to share with support:\r\n--------------------\r\n\r\n\r\n--------------------";
+        public const string ApplicationSupportEmailSubjectTemplate = "Feedback for {0} {1}";
+        public const string PromptRateApplicationEmailFeedbackMessage = "Would you like to send an e-mail with feedback instead?";
+        public const string PromptRateApplicationEmailFeedbackTitle = "Share feedback";
+        public const string PromptRateApplicationMessage = "Would you like to rate this application and provide feedback on how we can make things better?";
+        public const string PromptRateApplicationTitle = "Rate Application";
+        public const string TextCancel = "Cancel";
+        public const string TextCancellationRequested = "Canceled...";
+        public const string TextErrorGeneric = "An unexpected error has occured. We apologize for any inconveniences here and we will be working to resolve this soon.";
+        public const string TextListNoData = "No items to display.";
+        public const string TextLoading = "Loading...";
+        public const string TextMaybeLater = "Not right now";
+        public const string TextNo = "No";
+        public const string TextNoInternet = "No internet connection available.Connect to internet and try again.";
+        public const string TextNotApplicable = "N/A";
+        public const string TextOk = "OK";
+        public const string TextYes = "Yes";
+        public const string ViewTitlePrivacyPolicy = "Privacy Policy";
+        public const string ViewTitleTermsOfService = "Terms of Service";
+    }
+}
