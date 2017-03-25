8eabfff50b574f76c85ff790e3fa75275ce71b6c
Mohammed Adenwala <madenwal@microsoft.com>
2017-03-25 09:41:51 -0500

    Restructured project

--- UniversalTemplate/Source/AppFramework/Templates/Contoso.UI.Uwp/Views/ViewBase.cs
+++
@@ -1,8 +0,0 @@
-﻿using AppFramework.Core.ViewModels;
-
-namespace Contoso.UI.Views
-{
-    public abstract class ViewBase<TViewModel> : AppFramework.UI.Views.ViewBase<TViewModel> where TViewModel : IViewModel
-    {
-    }
-}
