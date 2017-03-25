8572aa55f2581f3605ffce6a1bacfb21eb1e3919
Mohammed Adenwala <madenwal@microsoft.com>
2017-03-25 00:42:56 -0500

    Bug fix with TryAgain button

--- UniversalTemplate/Source/AppFramework/AppFramework.UI.Uwp/Resources/Common.xaml.cs
+++ UniversalTemplate/Source/AppFramework/AppFramework.UI.Uwp/Resources/Common.xaml.cs
@@ -1,10 +1,20 @@
-﻿namespace AppFramework.UI.Resources
+﻿using AppFramework.Core.Models;
+using Windows.UI.Xaml.Controls;
+
+namespace AppFramework.UI.Resources
 {
     public partial class Common
     {
         public Common()
         {
             this.InitializeComponent();
+        }
+
+        private void btnTryAgain_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if(sender is Button btn)
+                if (btn.DataContext is INotifyTaskCompletion task)
+                    task.Refresh();
         }
     }
 }
