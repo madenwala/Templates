878476ba363252a94cd67236dc12d7f588501248
Mohammed Adenwala <madenwal@microsoft.com>
2017-04-01 10:47:09 -0500

    Updated build, references, etc, to make UI show from nuget package in test apps

--- AppTemplate/Source/AppFramework/Build/Build.cmd
+++ AppTemplate/Source/AppFramework/Build/Build.cmd
@@ -9,10 +9,8 @@ msbuild ..\..\AppFramework\AppFramework.UI.Uwp\AppFramework.UI.Uwp.csproj /p:Con
 msbuild ..\..\AppFramework\AppFramework.Core.Uwp\AppFramework.Core.Uwp.csproj /p:Configuration="Debug" /p:Platform="arm"
 msbuild ..\..\AppFramework\AppFramework.UI.Uwp\AppFramework.UI.Uwp.csproj /p:Configuration="Debug" /p:Platform="arm"
 
-xcopy ..\..\AppFramework\AppFramework.Core.Uwp\bin\x86\Debug\AppFramework.Core.Uwp.dll Temp\AppFramework.Core.Uwp.dll /Y
+xcopy ..\..\AppFramework\AppFramework.Core.Uwp\bin\x86\Debug\* Temp\* /Y
 corflags /32BITREQ- Temp\AppFramework.Core.Uwp.dll
-
-xcopy ..\..\AppFramework\AppFramework.UI.Uwp\bin\x86\Debug\AppFramework.UI.Uwp.dll Temp\AppFramework.UI.Uwp.dll /Y
 corflags /32BITREQ- Temp\AppFramework.UI.Uwp.dll
 
 Build-NuGet-Package.cmd
