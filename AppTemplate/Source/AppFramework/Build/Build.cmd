68c95c7ff6feca54560e95a90e047a5cad4b090a
Mohammed Adenwala <madenwal@microsoft.com>
2017-04-04 15:31:18 -0500

    Bug fix

--- AppTemplate/Source/AppFramework/Build/Build.cmd
+++ AppTemplate/Source/AppFramework/Build/Build.cmd
@@ -4,7 +4,7 @@ msbuild ..\..\AppFramework\AppFramework.Uwp\AppFramework.Uwp.csproj /p:Configura
 msbuild ..\..\AppFramework\AppFramework.Uwp\AppFramework.Uwp.csproj /p:Configuration="Release" /p:Platform="x86"
 msbuild ..\..\AppFramework\AppFramework.Uwp\AppFramework.Uwp.csproj /p:Configuration="Release" /p:Platform="arm"
 
-xcopy ..\..\AppFramework\AppFramework.Uwp\bin\x86\Debug\* Temp\* /Y
+xcopy ..\..\AppFramework\AppFramework.Uwp\bin\x86\Release\* Temp\* /Y
 corflags /32BITREQ- Temp\AppFramework.Uwp.dll
 
 Build-NuGet-Package.cmd
