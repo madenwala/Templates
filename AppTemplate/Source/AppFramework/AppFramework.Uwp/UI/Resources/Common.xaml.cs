c1b68d580c6c3d82310fb7d3aa595db998ad6ca6
Mohammed Adenwala <madenwal@microsoft.com>
2017-05-05 20:01:27 -0500

    WebView refactoring, AdaptiveListView now supports right-clicking context menu out of box

--- AppTemplate/Source/AppFramework/AppFramework.Uwp/UI/Resources/Common.xaml.cs
+++ AppTemplate/Source/AppFramework/AppFramework.Uwp/UI/Resources/Common.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using AppFramework.Core.Models;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
 
 namespace AppFramework.UI.Resources
 {
@@ -15,6 +17,16 @@ namespace AppFramework.UI.Resources
             if(sender is Button btn)
                 if (btn.DataContext is INotifyTaskCompletion task)
                     task.Refresh();
+        }
+
+        private void List_RightTapped(object sender, Windows.UI.Xaml.Input.RightTappedRoutedEventArgs e)
+        {
+            FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
+        }
+
+        private void List_Holding(object sender, Windows.UI.Xaml.Input.HoldingRoutedEventArgs e)
+        {
+            FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
         }
     }
 }
---
+++ AppTemplate/Source/AppFramework/AppFramework.Uwp/UI/Views/WebView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using AppFramework.Core;
+using AppFramework.Core.ViewModels;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Navigation;
+
+namespace AppFramework.UI.Views
+{
+    public abstract class WebViewBase : ViewBase<WebViewModel>
+    {
+    }
+
+    public sealed partial class WebView : WebViewBase
+    {
+        public WebView()
+        {
+            InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+
+        protected override Task OnLoadStateAsync(LoadStateEventArgs e)
+        {
+            if (e.NavigationEventArgs.NavigationMode == NavigationMode.New || this.ViewModel == null)
+            {
+                WebViewModel vm;
+                if (e.NavigationEventArgs.Parameter is WebViewModel wvm)
+                    vm = wvm;
+                else
+                    vm = new WebViewModel();
+                this.SetViewModel(vm);
+            }
+
+            return base.OnLoadStateAsync(e);
+        }
+    }
+}
