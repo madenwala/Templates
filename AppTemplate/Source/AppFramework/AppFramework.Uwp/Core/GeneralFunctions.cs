d68a357887dc9c8a6be0ef0df5625efd84572740
Mohammed Adenwala <madenwal@microsoft.com>
2017-06-13 14:56:01 -0500

    Lower case phone number letters are handled

--- AppTemplate/Source/AppFramework/AppFramework.Uwp/Core/GeneralFunctions.cs
+++ AppTemplate/Source/AppFramework/AppFramework.Uwp/Core/GeneralFunctions.cs
@@ -139,16 +139,17 @@ namespace AppFramework.Core
                 if (!string.IsNullOrWhiteSpace(phone))
                 {
                     phone = phone.ToUpper();
-                    phone = Regex.Replace(phone, "[ABC]", "2");
-                    phone = Regex.Replace(phone, "[DEF]", "3");
-                    phone = Regex.Replace(phone, "[GHI]", "4");
-                    phone = Regex.Replace(phone, "[JKL]", "5");
-                    phone = Regex.Replace(phone, "[MNO]", "6");
-                    phone = Regex.Replace(phone, "[PQRS]", "7");
-                    phone = Regex.Replace(phone, "[TUV]", "8");
-                    phone = Regex.Replace(phone, "[WXYZ]", "9");
+                    phone = Regex.Replace(phone, "[abcABC]", "2");
+                    phone = Regex.Replace(phone, "[defDEF]", "3");
+                    phone = Regex.Replace(phone, "[ghiGHI]", "4");
+                    phone = Regex.Replace(phone, "[jklJKL]", "5");
+                    phone = Regex.Replace(phone, "[mnoMNO]", "6");
+                    phone = Regex.Replace(phone, "[pqrsPQRS]", "7");
+                    phone = Regex.Replace(phone, "[tuvTUV]", "8");
+                    phone = Regex.Replace(phone, "[wxyzWXYZ]", "9");
                     phone = Regex.Replace(phone, "[)]", "-");
                     phone = Regex.Replace(phone, "[( ]", "");
+                    phone = Regex.Replace(phone, "[.]", "");
                     return phone.Trim();
                 }
             }
