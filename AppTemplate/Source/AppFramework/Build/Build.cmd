27847ae5721bea85148b9253c65bde47a974cb01
Mohammed Adenwala <adenwala@outlook.com>
2017-06-14 14:36:21 -0500

    Add PCL support to AppFramework

--- AppTemplate/Source/AppFramework/Build/Build.cmd
+++ AppTemplate/Source/AppFramework/Build/Build.cmd
@@ -1,6 +1,7 @@
 msbuild ..\..\AppFramework\AppFramework.Uwp\AppFramework.Uwp.csproj /p:Configuration="Release" /p:Platform="x64"
 msbuild ..\..\AppFramework\AppFramework.Uwp\AppFramework.Uwp.csproj /p:Configuration="Release" /p:Platform="x86"
 msbuild ..\..\AppFramework\AppFramework.Uwp\AppFramework.Uwp.csproj /p:Configuration="Release" /p:Platform="arm"
+msbuild ..\..\AppFramework\AppFramework.PCL\AppFramework.PCL.csproj /p:Configuration="Release" /p:Platform="Any CPU"
 
 xcopy ..\..\AppFramework\AppFramework.Uwp\bin\x86\Release\* Temp\* /Y
 corflags /32BITREQ- Temp\AppFramework.Uwp.dll
