b590869234ccb8eeabbd92b23776260a9bd5c5da
Mohammed Adenwala <madenwal@microsoft.com>
2018-04-12 16:12:42 -0500

    Added Xamarin projects

--- AppTemplate/Source/NuGetReferened/XamarinForms/Contoso.UI.Android/Properties/AssemblyInfo.cs
+++ AppTemplate/Source/NuGetReferened/XamarinForms/Contoso.UI.Android/Properties/AssemblyInfo.cs
@@ -1,18 +1,20 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Android.App;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Contoso.UI")]
+[assembly: AssemblyTitle("Contoso.UI.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Contoso.UI")]
+[assembly: AssemblyProduct("Contoso.UI.Android")]
 [assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -26,4 +28,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: ComVisible(false)]
+
+// Add some common permissions, these can be removed if not needed
+[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
--- AppTemplate/Source/NuGetReferened/XamarinForms/Contoso.UI.UWP/Properties/AssemblyInfo.cs
+++ AppTemplate/Source/NuGetReferened/XamarinForms/Contoso.UI.UWP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Contoso.UI")]
+[assembly: AssemblyTitle("Contoso.UI.UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Contoso.UI")]
+[assembly: AssemblyProduct("Contoso.UI.UWP")]
 [assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
